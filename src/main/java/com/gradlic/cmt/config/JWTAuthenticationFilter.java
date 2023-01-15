f1094518cac8206e8c04ee5f9fb4e9eaa8468d84
Vikas Yadav <v.ray96610@gmail.com>
2023-01-15 23:56:09 +0530

    Generate and validate jwt token.

--- src/main/java/com/gradlic/cmt/config/JWTAuthenticationFilter.java
+++ src/main/java/com/gradlic/cmt/config/JWTAuthenticationFilter.java
@@ -1,4 +1,28 @@
 package com.gradlic.cmt.config;
 
-public class JWTAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if (authHeader==null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+
+    }
 }
